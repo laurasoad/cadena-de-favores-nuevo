cd7d3792790da0328a48e9e46bb15316c118c928
Laurafarrerdev <laurafarrer1@gmail.com>
2022-04-12 00:13:43 +0200

    FRONT creado el Componente 'PublicationItem' en la carpeta publications-list

---
+++ front/src/pages/publications-list/PublicationItem.vue
@@ -0,0 +1,74 @@
+<template>
+  
+   <article class="pub-item">
+      <p>Title: {{ publication.title }}</p>
+      <p>Type: {{ publication.publication_type }}</p>
+      <p> >> {{ publication.categories }}</p>
+      <p>Location: {{ publication.location }}</p>
+      <button>
+         <router-link :to="`/publications/${publication.id_pub}`">Details</router-link>
+      </button>
+   </article>
+</template>
+
+<script>
+export default {
+  name: "PublicationItem",
+  props: {
+    publication: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+
+  .pub-item {
+    flex: 1 1 21rem;
+    border: 2px solid #04AA6D;
+    border-radius: 1em;
+    margin: 1em 2em;
+    padding: 1em 2em;
+    /*max-width: 10em;*/
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-self: normal;
+
+
+  }
+
+  .pub-item:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+  button {
+  border: 2px solid #04AA6D;
+  background-color: white;
+  color: black;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: #04AA6D;
+  color: white;
+  
+  text-shadow: 1px #837c7c;
+  border-radius: 0.5em;
+}
+
+button:hover {
+  color: #04AA6D;
+  background-color: white;
+  border-color: #04AA6D;
+
+  
+}
+
+router-link {
+    text-decoration:none;
+    color:#2c3e50
+}
+</style>
--- front/src/pages/publications-list/PublicationsListPage.vue
+++ front/src/pages/publications-list/PublicationsListPage.vue
@@ -1,27 +1,22 @@
 <template>
   
   <section class="publication-list">
-    <article class="pub-item" v-for=" publication in publication_list" :key="publication.id_pub">
-      <p>Title: {{ publication.title }}</p>
-      <p>Type: {{ publication.publication_type }}</p>
-      <p> >> {{ publication.categories }}</p>
-      <p>Location: {{ publication.location }}</p>
-    
-      <button>
-         <router-link :to="`/publications/${publication.id_pub}`">Details</router-link>
-      </button>
-     
-    </article>
+    <PublicationItem  v-for=" publication in publication_list" :key="publication.id_pub" 
+    :publication="publication"/>
   </section>
-  <pre>{{publications}}</pre>
+  <pre>{{publication_list}}</pre>
 </template>
 
 <script>
-  //  import { getPublications } from '@/services/api.js'
+  // mejora: import { getPublications } from '@/services/api.js'
+  import PublicationItem from "./PublicationItem.vue";
 
   export default {
      
     name: 'PublicationList',
+    components: {
+      PublicationItem
+    },
     data() {
       return {
         publication_list: [],
@@ -37,7 +32,7 @@
     methods: {
       async loadData() {
          
-        //getPublications
+        //mejora: getPublications en services/api.js
         const response = await fetch('http://localhost:5000/api/publications')
         this.publication_list = await response.json()
         
@@ -67,50 +62,5 @@
 
   }
 
-  .pub-item {
-    
-    flex: 1 1 21rem;
-    border: 2px solid #04AA6D;
-    border-radius: 1em;
-    margin: 1em 2em;
-    padding: 1em 2em;
-    /*max-width: 10em;*/
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-    align-self: normal;
 
-
-  }
-
-  .pub-item:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
-
-  button {
-  border: 2px solid #04AA6D;
-  background-color: white;
-  color: black;
-  padding: 14px 28px;
-  font-size: 16px;
-  cursor: pointer;
-  background-color: #04AA6D;
-  color: white;
-  
-  text-shadow: 1px #837c7c;
-  border-radius: 0.5em;
-}
-
-button:hover {
-  color: #04AA6D;
-  background-color: white;
-  border-color: #04AA6D;
-
-  
-}
-
-a:link, a:visited, a:active {
-    text-decoration:none;
-    color:#2c3e50
-}
 </style>
