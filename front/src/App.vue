8de625fa57cd154c4bd08bb77189399845008dec
laurasoad <laurafarrer1@gmail.com>
2022-06-15 22:09:52 +0200

    FRONT App.vue FEATURE muestra si hay un usuario conectado

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,6 +2,38 @@
   <div id="nav">
     <router-link to="/">Home</router-link>|
     <router-link to="/about">About</router-link>
+  <p v-if="this.activeUser != null"> Usuario conectado: {{activeUser.user_id}} - {{activeUser.first_name}} {{activeUser.last_name}}</p>  
+    <p v-else> Usuario no conectado</p>  
+
   </div>
   <router-view />
 </template>
+<script>
+export default {
+  data() {
+    return {
+      // activeUser : JSON.parse(localStorage.getItem("activeUserWatcher")),
+      activeUser : {},      
+
+    };
+  },
+  
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+     
+      // cargar id usuario activo
+      this.activeUser = JSON.parse(localStorage.getItem("activeUserWatcher"))
+
+      
+
+    },
+  }
+};
+
+
+  
+  
+</script>
