02ec5b1b2bbb175bca4f8072702ea0ccf342faec
laurasoad <laurafarrer1@gmail.com>
2022-06-16 07:57:15 +0200

    FRONT PublicationsList añadido filtro de búsqueda por título

--- front/src/pages/publications-list/PublicationsListPage.vue
+++ front/src/pages/publications-list/PublicationsListPage.vue
@@ -1,9 +1,14 @@
 <template>
-  
-  <section class="publication-list">
-    <PublicationItem  v-for=" publication in publication_list" :key="publication.id_pub" 
-    :publication="publication"/>
-  </section>
+  <div class="container">
+    <section class="wrap-search">
+      <input id="search" type="search" placeholder="Buscar..." v-model="searchData"/>
+    </section>
+      <br>
+     <section class="publication-list">
+      <PublicationItem  v-for=" publication in filteredData" :key="publication.id_pub" 
+      :publication="publication"/>
+     </section>
+  </div>
 </template>
 
 <script>
@@ -21,7 +26,8 @@
     },
     data() {
       return {
-        publication_list: [],
+        publicationList: [],
+        searchData : ""
       }
     },
     mounted() {
@@ -29,19 +35,54 @@
     },
     methods: {
       async loadData() {
-        this.publication_list = await getPublications();
-      },
+        this.publicationList = await getPublications();
+      }
+      
+    },
+      computed: {
+        filteredData(){
+          
+      return this.publicationList.filter((element) => (element.title.toLowerCase().includes(this.searchData.toLowerCase())))
     }
+   
+
+  }
 
 
   }
 </script>
 
 <style scoped>
+
   body {
     margin: 0 auto;
-  }
+    /** */
+    font-family: 'Nunito Sans';
+    background-color: #f2f2f2;
+    }
+  
 
+    #search{
+      background-color: #fbfbfb; 
+      width: 75%; 
+
+      height: 2.5em; 
+      border: 1px solid black;
+      border-radius: 5px;  
+      padding-left: 0.1em;
+      margin-top: 0.2em;  
+      margin-bottom: 0.8em; 
+      padding: 1.5em;
+    }
+    #search:hover {
+      border: 1.5px solid black;
+      /** 
+      box-shadow: 0 7px 15px 0 rgba(0,0,0,0.1);
+
+       */
+
+
+    }
 
   h1 {
     font-style: italic;
