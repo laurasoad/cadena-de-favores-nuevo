b8010130b7affbe40096b10e9b89bbbe4a837816
laurafarrerdev <laurafarrer1@gmail.com>
2022-06-19 23:45:18 +0200

    PublicationItem  1)Pubs Buscar y Ofrecer Ayuda tienen distinto Color + 2) Creado <button> unido método goToDetailsClicked (redirige por id) y eliminado <button> con <router-link>

--- front/src/pages/publications-list/PublicationItem.vue
+++ front/src/pages/publications-list/PublicationItem.vue
@@ -1,11 +1,14 @@
 <template>
-   <article class="pub-item">
-      <p>Título: {{ publication.title }}</p>
-      <p>Tipo de publicación: {{ publication.publication_type }}</p>
-      <p> Categoría: {{ publication.categories }}</p>
-      <p>Lugar: {{ publication.location }}</p>
-      <button>
-      <router-link :to="`/publications/${publication.id_pub}`">Details</router-link>
+   <article class="pub-item" v-bind:class="{seek: seekHelpStyle, offer: offerHelpStyle}">
+      <section>
+        <h4>{{ publication.title }}</h4>
+        <p> Categoría: {{ publication.categories }}</p>
+        <p>Lugar: {{ publication.location }}</p>
+
+      </section>
+
+      <button @click="goToDetailsClicked">
+        Detalles 
       </button>
    </article>
 </template>
@@ -19,35 +22,116 @@ export default {
       required: true,
     },
   },  
-
-  /**
-    <button @click="goToDetailsClicked">
-        Detalles
-      
-      </button>
-      
-  methods: {
-    goToDetailsClicked() { 
-      console.log("---> onGoToDetailsClicked()"
-   
-     // this.$router.push({ name: 'user', path : '/publications/:id', params: { publication.id_pub: `${publication.id_pub} } })
-
-     
+  data() {
+      return {
+        seekHelpStyle: false,
+        offerHelpStyle: false      }
     },
-  },
+    mounted() {
+      this.loadStyles()
+    },
+    methods: {
+      loadStyles() {
+        if (this.publication.publication_type == 0){
+          this.seekHelpStyle = true
+        }
+        if (this.publication.publication_type == 1){
+          this.offerHelpStyle = true
+        }
+      },
+      goToDetailsClicked() { 
+      console.log("---> onGoToDetailsClicked()")
+   
+      this.$router.push({ name: 'PublicationDetail', params: { id: `${this.publication.id_pub}`} } )
+      }
+
+    },
   
   
-  */
+  
+  
 
 };
 </script>
 
 <style scoped>
+  /**
+  Sombra chula sin color
+    .pub-item:hover {
+       box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+  
+  No funciona el root en Vue
+  :root {
+    --main-offer-color:   rgb(90, 104, 212);
+    --main-seek-color: rgb(212, 130, 48);
+  }
+  */
+  .offer {
+    --main-offer-color:  rgb(90, 104, 212);
+    color:   var(--main-offer-color);
+    border: 2px solid  var(--main-offer-color);
+  }
+
+  
+  .offer:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+
+  .offer button {
+    --main-offer-color:    rgb(90, 104, 212);
+    border: 2px solid  var(--main-offer-color);
+    background-color: white;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+    background-color: var(--main-offer-color);
+    color: white;  
+    text-shadow: 1px #837c7c;
+    border-radius: 0.5em;
+
+  }
+
+  
+  .offer button:hover {
+    --main-offer-color:    rgb(90, 104, 212);
+    color: var(--main-offer-color);
+    background-color: white;
+    border-color:  var(--main-offer-color); 
+  }
+
+  .seek {
+    color: -main-seek-color;
+    border: 2px solid -main-seek-color;
+  }
+  .seek:hover {
+    box-shadow: 0 8px 16px 0 rgba(182, 17, 17, 0.2);
+  }
+
+  .seek button {
+    border: 2px solid  rgb(212, 130, 48);
+    background-color: white;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+    background-color: rgb(212, 130, 48);
+    color: white;
+    text-shadow: 1px #837c7c;
+    border-radius: 0.5em;
+  }
+
+  
+  .seek button:hover {
+    color: rgb(212, 130, 48);
+    background-color: white;
+    border-color: rgb(212, 130, 48);
+  }
 
 
   .pub-item {
     flex: 1 1 21rem;
-    border: 2px solid #04AA6D;
+    /* border: 2px solid #04AA6D; 
+    **/
     border-radius: 1em;
     margin: 1em 2em;
     padding: 1em 2em;
@@ -56,19 +140,17 @@ export default {
     flex-direction: column;
     align-self: normal;
     max-width: 14em;
+    /**falta añadir flex para que el espacio se reparta entre section y button */
 
 
   }
-  
 
-  .pub-item:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
+
+
 
   button {
   border: 2px solid #04AA6D;
   background-color: white;
-  color: black;
   padding: 14px 28px;
   font-size: 16px;
   cursor: pointer;
@@ -86,9 +168,6 @@ button:hover {
 
   
 }
-a:link, a:visited, a:active {
-    text-decoration:none;
-      color:#2c3e50
-}
+
 
 </style>
