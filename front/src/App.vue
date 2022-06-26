55d0f76181ff56f8904f9284ac860533b6710efa
laurasoad <laurafarrer1@gmail.com>
2022-06-26 20:30:05 +0200

    FRONT App.vue FIX Se puede ver si hay un Usuario conectado + Creado link a Dashboard en Navegador

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,19 +1,27 @@
-<template>
+
+<template >
   <div id="nav">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>
-  <p v-if="this.activeUser != null"> Usuario conectado: {{activeUser.user_id}} - {{activeUser.first_name}} {{activeUser.last_name}}</p>  
+
+    <router-link to="/">Home</router-link> | 
+    <router-link to="/about">About</router-link> | 
+    <router-link v-if="this.activeUser != null" to="/dashboard">Dashboard</router-link>
+
+    <p v-if="this.activeUser != null"> 
+  Usuario conectado: {{activeUser.user_id}} - {{activeUser.first_name}} {{activeUser.last_name}}</p>  
     <p v-else> Usuario no conectado</p>  
+
 
   </div>
   <router-view />
 </template>
+
+
 <script>
+
 export default {
   data() {
     return {
-      // activeUser : JSON.parse(localStorage.getItem("activeUserWatcher")),
-      activeUser : {},      
+      activeUser : null,      
 
     };
   },
@@ -22,21 +30,34 @@ export default {
     this.loadUser();
     
   },
+    watch: {
+    activeUser:{
+      handler(newActiveUser){
+        console.log("cambio detectado desde App.vue")
+        localStorage.activeUserWatcher = JSON.stringify(newActiveUser);
+      },
+      deep: true
+    }
+  },
   methods: {
     loadUser() {
            //  window.localStorage.removeItem('activeUserWatcher')
-        //window.localStorage.setItem('activeUserWatcher')
+        //window.localStorage.setItem('activeUserWatcher', JSON.stringify(this.selectedUser))
       // cargar id usuario activo
-      this.activeUser = JSON.parse(localStorage.getItem("activeUserWatcher"))
-    /**
+        let extraer = JSON.parse(localStorage.getItem('activeUserWatcher'))
+       // let enviar = localStorage.setItem('activeUserWatcher', JSON.stringify(this.selectedUser))
         if (localStorage.getItem('activeUserWatcher')) {
+          console.log("extraer ", extraer) // si no hay, es null <___________
+      //    console.log(enviar)
       try {
+        // extraer GET
         this.activeUser = JSON.parse(localStorage.getItem('activeUserWatcher'));
       } catch(e) {
-        localStorage.removeItem('activeUserWatcher');
-      }
-    }
-     */
+        console.log("error, cual?", e)
+        //  localStorage.removeItem('activeUserWatcher');
+      }}
+      
+
    
     },
   }
