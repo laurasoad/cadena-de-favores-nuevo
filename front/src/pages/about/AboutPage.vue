b67d6534065a3edf1bb0c2534f14445eba65b96d
laurasoad <laurafarrer1@gmail.com>
2022-06-26 19:17:27 +0200

    FRONT AboutPage añadida img

--- front/src/pages/about/AboutPage.vue
+++ front/src/pages/about/AboutPage.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-        <img class="logo" alt="Help logo" src="@/assets/img/helplogo.png" />
+    <h1>About us</h1>
+    <!--
         <img class="mountain" alt="Mountain logo" src="@/assets/img/helping-hand-silhouette-mountain-moonlight.jpg" />´
+          <section>
+      <h3>
+        ¿Quiénes somos?
+      </h3>
+    </section>
+
+    -->
+   <img class="dog" alt="About us - dog in the keyboards" src="https://images.unsplash.com/photo-1589652717406-1c69efaf1ff8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />´
+  
 
 
   </div>
@@ -11,14 +20,20 @@
 export default {
   data() {
   // otro: "{ backgroundImage: 'url(' + require(`@/assets/${imagepath}`) + ')', width: '300px', height: '200px' }"
+  
+  //      <img class="mountain" alt="Mountain logo" src="@/assets/img/helping-hand-silhouette-mountain-moonlight.jpg" />´
+
   },
 }
 </script>
 
 <style scoped>
 body { 
+  /**
+      background-image: url('~@/assets/img/helping-hand-silhouette-mountain-moonlight.jpg'); 
+    background-image: url('./src/assets/img/logo.png'); 
 
-    background-image: url('~@/assets/img/helping-hand-silhouette-mountain-moonlight.jpg'); 
+  */
   
 }
 .logo {
@@ -28,7 +43,7 @@ body {
   max-width: 9em;
   
 }
-.mountain {
+.dog {
   border-radius: 5px;
   max-height: 30em;
   max-width: 30em;
